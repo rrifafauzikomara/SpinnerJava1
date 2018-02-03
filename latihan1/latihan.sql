371a7a1f9f85062fc2c5a60f64afb61aa7f6bfad
rrifafauzikomara <rifafauzi6@gmail.com>
2018-02-04 01:00:22 +0700

    New Updated

---
+++ latihan1/latihan.sql
@@ -0,0 +1,57 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 03 Feb 2018 pada 18.50
+-- Versi Server: 10.1.28-MariaDB
+-- PHP Version: 7.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `latihan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kendaraan`
+--
+
+CREATE TABLE `kendaraan` (
+  `id_kendaraan` varchar(5) NOT NULL,
+  `kode_kendaraan` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `kendaraan`
+--
+
+INSERT INTO `kendaraan` (`id_kendaraan`, `kode_kendaraan`) VALUES
+('K001', 'Bus Bisnis'),
+('K002', 'Bus Ace');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `kendaraan`
+--
+ALTER TABLE `kendaraan`
+  ADD PRIMARY KEY (`id_kendaraan`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ latihan1/menu.php
@@ -0,0 +1,16 @@
+<?php 
+
+	 include_once "koneksi.php";
+
+	 $query = mysqli_query($con, "SELECT * FROM kendaraan ORDER BY id_kendaraan ASC");
+	
+	 $json = array();	
+	
+	 while($row = mysqli_fetch_assoc($query)){
+	 	$json[] = $row;
+	 }
+	
+	 echo json_encode($json);
+	
+	 mysqli_close($con);
+?>
